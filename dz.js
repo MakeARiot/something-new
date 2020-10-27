777379414d2082adb2d1a1a2293ef6891e3c6da5
kirill <kkramarskiy@gmail.com>
2020-10-27 13:43:27 +0400

    +

--- dz.js
+++ dz.js
@@ -24,28 +24,28 @@ console.log(calc("1", "+", 2));
 // 2. Функция получает первым параметром массив, каждый элемент которого является строкой. 
 // Вторым параметром подстроку для поиска. Возвращает количество элементов массива, которые содержат переданную подстроку.
 // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-function find(str, ...arr){
-    let counter = 0;
-    arr.forEach((i, item)=>{
-        if (i[item] == str) counter++;
-        console.log(i[item]);
-    })
-    return counter;
-}
+// function find(str, ...arr){
+//     let counter = 0;
+//     arr.forEach((i, item)=>{
+//         if (i[item] == str) counter++;
+//         console.log(i[item]);
+//     })
+//     return counter;
+// }
 
 // 3. Функция получает число и возвращает строку с отформатированным значением вида «(-) 00 000 000,00». 
 // Разделитель разрядов – пробел, разделитель целой и дробной части - запятая. Количество знаков после запятой – от 0 до 2-х.
 //
-function num_format(num){
-    let str_num = "" + num;
-    let counter = 0;
-    for(let i=str_num.length-1;i>0;i--){
-        if (i % 3 == 0){
-            console.log(str_num[i]);
-            str_num[i] = " " + str_num[i];
-        }
-    }
-    return str_num;
-}
+// function num_format(num){
+//     let str_num = "" + num;
+//     let counter = 0;
+//     for(let i=str_num.length-1;i>0;i--){
+//         if (i % 3 == 0){
+//             console.log(str_num[i]);
+//             str_num[i] = " " + str_num[i];
+//         }
+//     }
+//     return str_num;
+// }
 
 // console.log(num_format(12345678));
